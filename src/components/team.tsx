323f071345a743af60ddb70379106b8d37c50224
Wael Hourani <wael.business@outlook.com>
2024-07-12 16:46:02 +1000

    commit

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -13,46 +13,23 @@ const Team = () => {
         alt=""
         className="w-[50px] border-4 border-lavender rounded-full"
       />
-      <img
-        src="/team/Jordan.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender rounded-full"
-      />
-      <img
-        src="/team/abbeir.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender rounded-full"
-      />
-      <img
-        src="/team/Maryam.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender rounded-full"
-      />
-      <img
-        src="/team/Layla.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender rounded-full"
-      />
-      <img
-        src="/team/Corey.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender rounded-full"
-      />
-      <img
-        src="/team/Tahlia.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender rounded-full"
-      />
-      <img
-        src="/team/Dancia.jpg"
-        alt=""
-        className="w-[50px] border-4 border-lavender h-[50px] rounded-full"
-      />
-      <img
-        src="/team/Zeinah.jpg"
+        <img
+        src="/khadeja.png"
         alt=""
         className="w-[50px] border-4 border-lavender h-[50px]  rounded-full"
       />
+     
+      <img
+        src="/Carlos.webp"
+        alt=""
+        className="w-[50px] border-4 border-lavender rounded-full"
+      />
+      <img
+        src="/marwa1.png"
+        alt=""
+        className="w-[50px] border-4 border-lavender h-[50px]  rounded-full"
+      />
+    
     </div>
   );
 };
