099c61a41605ca4e31358bcc47b062f7e1218be4
Wael Hourani <wael.business@outlook.com>
2024-08-04 12:32:26 +1000

    commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+        stream: false,
+        http: false,
+        https: false,
+        zlib: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
