2758d7cf5fcafcff99d8d4e49369bc17c4784a89
Wael Hourani <wael.business@outlook.com>
2024-08-04 12:47:04 +1000

    commit

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,13 +1,14 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { NextResponse } from 'next/server';
 
+// Configure Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const runtime = 'edge';
+export const runtime = 'nodejs'; // Ensure this runs on Node.js environment
 
 export async function POST(req) {
     const formData = await req.formData();
